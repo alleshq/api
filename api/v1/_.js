c72b5ceda75202827cd39fd6e459b1e002210c61
Archie Baer <archie@abaer.dev>
2020-04-09 02:42:49 +0100

    Added "user" endpoint

--- api/v1/_.js
+++ api/v1/_.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/token", applicationAuth, require("./token"));
 router.get("/token", tokenAuth, require("./tokenInfo"));
 router.get("/me", tokenAuth, require("./me"));
+router.get("/user", applicationAuth, require("./user"));
 
 module.exports = router;
