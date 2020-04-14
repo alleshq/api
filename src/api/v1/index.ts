ad37683ebfd6aef886325665e202f8b94d748622
Dante <dante@issaias.net>
2020-04-14 11:57:21 +0100

    Fix issue with routes and require
    Signed-off-by: Dante <dante@issaias.net>

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -3,9 +3,9 @@ import applicationAuth from "../../util/applicationAuth";
 import tokenAuth from "../../util/tokenAuth";
 
 const router = express.Router();
-router.post("/token", applicationAuth, require("./token"));
-router.get("/token", tokenAuth, require("./tokenInfo"));
-router.get("/me", tokenAuth, require("./me"));
-router.get("/user", applicationAuth, require("./user"));
+router.post("/token", applicationAuth, require("./token").default);
+router.get("/token", tokenAuth, require("./tokenInfo").default);
+router.get("/me", tokenAuth, require("./me").default);
+router.get("/user", applicationAuth, require("./user").default);
 
 export default router;
