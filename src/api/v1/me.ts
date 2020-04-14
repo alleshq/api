c8397a5ef8c3fbefb72d53477be7b442700294f4
Dante <dante@issaias.net>
2020-04-14 10:54:36 +0100

    Create team interface
    Signed-off-by: Dante <dante@issaias.net>

--- src/api/v1/me.ts
+++ src/api/v1/me.ts
@@ -1,7 +1,14 @@
 import {Request, Response} from "express";
+import {Team} from "../../types";
 
 export default async (req: Request, res: Response) => {
-	const responseData: any = {
+	const responseData: {
+		id: string;
+		username: string;
+		name: string;
+		nickname: string;
+		teams?: Team[];
+	} = {
 		id: req.user.id,
 		username: req.user.username,
 		name: req.user.name,
@@ -10,11 +17,13 @@ export default async (req: Request, res: Response) => {
 
 	//Team List
 	if (req.token.scopes.includes("teams")) {
-		responseData.teams = (await req.user.getTeams()).map(team => ({
-			id: team.id,
-			slug: team.slug,
-			name: team.name
-		}));
+		responseData.teams = (await req.user.getTeams()).map(
+			(team: Team): Team => ({
+				id: team.id,
+				slug: team.slug,
+				name: team.name
+			})
+		);
 	}
 
 	res.json(responseData);
